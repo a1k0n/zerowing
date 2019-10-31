dc5f2361176e02260ed0042a3c3967981438adc8
Andy Sloane <andy@a1k0n.net>
2019-10-30 22:12:11 -0700

    use stm8flash instead of openocd; add full stm8.h header

--- Makefile
+++ Makefile
@@ -4,17 +4,19 @@ CFLAGS = -lstm8 -mstm8 --std-sdcc11
 CFILES = main.c
 
 
-all:: main.elf
+all:: main.hex
 
-main.elf: $(CFILES)
-	$(CC) $(CFLAGS) --out-fmt-elf -o $@ $(CFILES)
+main.hex: $(CFILES)
+	# $(CC) $(CFLAGS) --out-fmt-elf -o $@ $(CFILES)
+	$(CC) $(CFLAGS) -o $@ $(CFILES)
 
-flash: main.elf
-	openocd -f interface/stlink.cfg -f target/stm8s003.cfg -c "program main.elf verify reset exit"
+flash: main.hex
+	# openocd -f interface/stlink.cfg -f target/stm8s003.cfg -c "program main.elf verify reset exit"
+	stm8flash -c stlinkv2 -p stm8s003f3 -w main.hex
 
 reformat:
 	# requires very recent clang-format for AlignConsecutiveMacros
 	clang-format -style="{BasedOnStyle: Google, AlignConsecutiveAssignments: true, AlignConsecutiveDeclarations: true, AlignConsecutiveMacros: true}" -i $(CFILES)
 
 clean:
-	rm -f main.asm main.cdb main.elf main.lk main.lst main.map main.rel main.rst main.sym
+	rm -f main.asm main.cdb main.elf main.lk main.lst main.map main.rel main.rst main.sym main.hex
--- main.c
+++ main.c
@@ -1,45 +1,14 @@
 #include <stdint.h>
+#include "stm8.h"
 
 #define F_CPU           2000000UL
-#define _SFR_(mem_addr) (*(volatile uint8_t *)(0x5000 + (mem_addr)))
-
-/* PORT A */
-#define PA_ODR _SFR_(0x00)
-#define PA_IDR _SFR_(0x01)
-#define PA_DDR _SFR_(0x02)
-#define PA_CR1 _SFR_(0x03)
-#define PA_CR2 _SFR_(0x04)
 
 #define BLUE_LED  1
 #define GREEN_LED 2
 
-/* TIM4 */
-#define TIM4_CR1  _SFR_(0x340)
-#define TIM4_IER  _SFR_(0x343)
-#define TIM4_SR   _SFR_(0x344)
-#define TIM4_EGR  _SFR_(0x345)
-#define TIM4_CNTR _SFR_(0x346)
-#define TIM4_PSCR _SFR_(0x347)
-#define TIM4_ARR  _SFR_(0x348)
-
-/* UART */
-#define UART_SR   _SFR_(0x230)
-#define UART_TXE  7
-#define UART_TC   6
-#define UART_RXNE 5
-#define UART_DR   _SFR_(0x231)
-#define UART_BRR1 _SFR_(0x232)
-#define UART_BRR2 _SFR_(0x233)
-#define UART_CR1  _SFR_(0x234)
-#define UART_CR2  _SFR_(0x235)
-#define UART_TEN  3
-#define UART_REN  2
-
-#define TIM4_ISR 23
-
-void timer_isr(void) __interrupt(TIM4_ISR) {
-  PA_ODR ^= (1 << BLUE_LED);
-  TIM4_SR &= ~1;
+void TIM4_ovf(void) __interrupt(TIM4_OVR_UIF_IRQ) {
+  PA_ODR ^= (1 << GREEN_LED);
+  TIM4_SR &= ~TIM_SR1_UIF;
 }
 
 static inline void delay_ms(uint16_t ms) {
@@ -54,13 +23,15 @@ int main() {
   /* Prescaler = 128 */
   TIM4_PSCR = 0b00000111;
   /* Period ~= 30Hz */
-  TIM4_ARR = 255;
-  TIM4_IER |= 1;  // Enable Update Interrupt
-  TIM4_CR1 |= 1;  // Enable TIM4
+  TIM4_ARR = 250;
+  TIM4_IER |= TIM_IER_UIE;  // Enable Update Interrupt
+  TIM4_CR1 |= TIM_CR1_CEN;  // Enable TIM4
 
+  rim();
   while (1) {
     /* toggle pin every 250ms */
-    PA_ODR ^= (1 << GREEN_LED);
+    // wfi();
     delay_ms(250);
+    PA_ODR ^= (1 << BLUE_LED);
   }
 }
